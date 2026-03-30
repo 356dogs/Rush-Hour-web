9584a2a6fe62e1c71e3677df4787a90f4f416c7b
Dimitry <didyblckm@gmail.com>
2026-03-30 11:20:36 +0200

    commencement de la logique js et de l'affichage html

---
+++ Gestion_Grille.js
@@ -0,0 +1,11 @@
+
+
+//js n'a pas de fonction randint(min,max) prédefinie donc on doit la creer
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function creation_grille(difficulte)
+{
+    
+}
